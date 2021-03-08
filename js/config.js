2a040244a1bf465cbf2522e0ef1b476611613cc7
Nic McPhee <mcphee@morris.umn.edu>
2021-03-08 14:46:58 -0600

    Add the Cognito info to the config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_NHM2B0c8z', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '202tcvo9rmgo1qudb4d0p66gqv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://puby08jq6k.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
