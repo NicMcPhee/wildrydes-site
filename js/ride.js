e7b1cb4d4d6f670105b3f04632778a2ba92c3dd8
Nic McPhee <mcphee@morris.umn.edu>
2021-03-08 13:53:17 -0600

    Revert "Comment out all the authentication stuff"
    This reverts commit ef13794692fd96829ff7ddcd7bf2c905dc5c5a7d.

--- js/ride.js
+++ js/ride.js
@@ -4,23 +4,23 @@ var WildRydes = window.WildRydes || {};
 WildRydes.map = WildRydes.map || {};
 
 (function rideScopeWrapper($) {
-    // var authToken;
-    // WildRydes.authToken.then(function setAuthToken(token) {
-    //     if (token) {
-    //         authToken = token;
-    //     } else {
-    //         window.location.href = '/signin.html';
-    //     }
-    // }).catch(function handleTokenError(error) {
-    //     alert(error);
-    //     window.location.href = '/signin.html';
-    // });
+    var authToken;
+    WildRydes.authToken.then(function setAuthToken(token) {
+        if (token) {
+            authToken = token;
+        } else {
+            window.location.href = '/signin.html';
+        }
+    }).catch(function handleTokenError(error) {
+        alert(error);
+        window.location.href = '/signin.html';
+    });
     function requestUnicorn(pickupLocation) {
         $.ajax({
             method: 'POST',
             url: _config.api.invokeUrl + '/ride',
             headers: {
-                // Authorization: authToken
+                Authorization: authToken
             },
             data: JSON.stringify({
                 PickupLocation: {
@@ -58,12 +58,12 @@ WildRydes.map = WildRydes.map || {};
         $('#request').click(handleRequestClick);
         $(WildRydes.map).on('pickupChange', handlePickupChanged);
 
-        // WildRydes.authToken.then(function updateAuthMessage(token) {
-        //     if (token) {
-        //         displayUpdate('You are authenticated. Click to see your <a href="#authTokenModal" data-toggle="modal">auth token</a>.');
-        //         $('.authToken').text(token);
-        //     }
-        // });
+        WildRydes.authToken.then(function updateAuthMessage(token) {
+            if (token) {
+                displayUpdate('You are authenticated. Click to see your <a href="#authTokenModal" data-toggle="modal">auth token</a>.');
+                $('.authToken').text(token);
+            }
+        });
 
         if (!_config.api.invokeUrl) {
             $('#noApiMessage').show();
